66e87e23cf522550d891af27e4fe32f56274e74f
Akhilesh Gautam <akhileshgautam19@gmail.com>
2026-02-17 09:22:53 +0530

    Cleaned project and created test controller

---
+++ Controllers/TestController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Learning_Concepts_of_ASPdotNET_Core.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Controller reached successfully!");
+        }
+    }
+}
