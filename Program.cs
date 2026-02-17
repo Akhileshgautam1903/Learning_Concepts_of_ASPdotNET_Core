e90d4d950f58fb0b6f579e6f57a20e77f40afdb8
Akhilesh Gautam <akhileshgautam19@gmail.com>
2026-02-17 09:40:37 +0530

    Created Inline Middleware (.Use and .Run)

--- Program.cs
+++ Program.cs
@@ -6,6 +6,22 @@ builder.Services.AddControllers();
 
 var app = builder.Build();
 
+app.Use(async (context, next) =>
+{
+    Console.WriteLine("Middleware 1: Before");
+    await next(context);
+    Console.WriteLine("Middleware 1: After");
+});
+
+app.Use(async (context, next) =>
+{
+    Console.WriteLine("Middleware 2: Before");
+    await next(context);
+    Console.WriteLine("Middleware 2: After");
+});
+
+//app.Run(async (context) => await context.Response.WriteAsync("Terminated successfully!!"));
+
 app.MapControllers();
 
 app.Run();
