b566705d149beb29e6220fe2877765a86b6542e0
Akhilesh Gautam <akhileshgautam19@gmail.com>
2026-02-18 23:49:17 +0530

    Added Secure Controller for protected route

---
+++ Controllers/SecureControllercs.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Learning_Concepts_of_ASPdotNET_Core.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SecureControllercs: ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Secret()
+        {
+            return Ok("You accessed protected route using COOKIE Auth");
+        }
+    }
+}
