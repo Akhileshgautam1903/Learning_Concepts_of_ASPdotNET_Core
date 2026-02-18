0305ba92e7db48ffd58dc7282a2d73023c340664
Akhilesh Gautam <akhileshgautam19@gmail.com>
2026-02-18 23:13:27 +0530

    Secure Controller created, can be accessed only if jwt token present in request

---
+++ Controllers/SecureController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Learning_Concepts_of_ASPdotNET_Core.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SecureController : ControllerBase
+    {
+        [Authorize]
+        [HttpGet("data")]
+        public IActionResult GetSecureData()
+        {
+            return Ok("This is protected data accessible only with a valid JWT.");
+        }
+    }
+}
